0048c92e77ac1e71a73e42d8b5aeee59bc535cd1
RaffaelVeneh <raffaelvincent.2024@student.uny.ac.id>
2025-06-23 22:14:40 +0700

    feat: Initial Django setup with Project model

---
+++ projects/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_projects')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
